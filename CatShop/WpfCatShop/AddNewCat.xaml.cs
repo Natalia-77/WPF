02e351483ce7f38178379953b48b38250ac3115a
vlasnata80@gmail.com <vlasnata80@gmail.com>
2021-03-29 23:37:23 +0300

    Add price

--- CatShop/WpfCatShop/AddNewCat.xaml.cs
+++ CatShop/WpfCatShop/AddNewCat.xaml.cs
@@ -6,15 +6,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using WpfCatShop.Helper;
 using Path = System.IO.Path;
 
--- CatShop/WpfCatShop/MainWindow.xaml.cs
+++ CatShop/WpfCatShop/MainWindow.xaml.cs
@@ -2,22 +2,10 @@
 using CatShop.Domain;
 using CatShop.EFData;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfCatShop
 {
